f1d185f8ca2f24838cc271592fcc5415e591f1d2
BaoBB <BaoBB@DESKTOP-6B8J260>
2021-06-30 10:50:10 +0700

    DAO: department, discount, hotel, place

---
+++ src/main/java/com/hakunamatata/springmvc/repository/DepartmentDAO.java
@@ -0,0 +1,39 @@
+package com.hakunamatata.springmvc.repository;
+
+import java.util.List;
+
+import com.hakunamatata.springmvc.entity.Department;
+
+public abstract class DepartmentDAO implements DAO<Department> {
+
+	@Override
+	public void insert(Department vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Department vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department get(Department vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> list(Department vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/hakunamatata/springmvc/repository/DiscountDAO.java
@@ -0,0 +1,41 @@
+package com.hakunamatata.springmvc.repository;
+
+import java.util.List;
+
+import com.hakunamatata.springmvc.entity.Discount;
+
+public class DiscountDAO implements DAO<Discount> {
+
+	@Override
+	public void insert(Discount vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Discount vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Discount vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Discount get(Discount vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Discount> list(Discount vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
