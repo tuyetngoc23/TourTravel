c2d56846b514ff90d225c57ffd774e2ecaf019ba
Administrator <Administrator@Administrator>
2021-07-02 00:11:17 +0700

    blog&user DAO

--- src/main/java/com/hakunamatata/springmvc/repository/impl/BlogDAO.java
+++ src/main/java/com/hakunamatata/springmvc/repository/impl/BlogDAO.java
@@ -1,107 +1,48 @@
 package com.hakunamatata.springmvc.repository.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
 import com.hakunamatata.springmvc.entity.Blog;
 import com.hakunamatata.springmvc.repository.DAO;
 
 
-@Repository("blogDAO")
+@Repository
 public class BlogDAO implements DAO<Blog> {
-	private static final Logger logger = 
-			LoggerFactory.getLogger(BlogDAO.class);
-	
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	// SQL
-		private final String BLOG_INSERT= "insert into blog (title,wname,content) values (?,?,?)";
-		private final String BLOG_UPDATE= "update blog set title=?,name=?, content=?  where id=?";
-		private final String BLOG_DELETE= "delete from blog where id=?";//use primary key
-		private final String BLOG_GET= "select * from blog where id=?";
-		private final String BLOG_LIST= "select * from blog";
-	
+	private SqlSessionTemplate session;
+
 	@Override
 	public void insert(Blog vo) {
-		logger.info("insert : "+vo);
-		jdbcTemplate.update(BLOG_INSERT, 
-						vo.getTitle(), vo.getContent(), vo.getWname());
-		
+		// TODO Auto-generated method stub
+		session.insert("Blogger.insert", vo);		
 	}
 
 	@Override
 	public void update(Blog vo) {
-		logger.info("update :"+vo);
-		jdbcTemplate.update(BLOG_UPDATE, 
-				vo.getTitle(), vo.getContent(), vo.getWname(),
-				vo.getId());
-		
+		// TODO Auto-generated method stub
+		session.update("Blogger.update", vo);
 	}
 
 	@Override
 	public void delete(Blog vo) {
-		logger.info("delete :"+vo);
-		jdbcTemplate.update(BLOG_DELETE, vo.getId());
-		
+		// TODO Auto-generated method stub
+		session.delete("Blogger.delete", vo);
 	}
 
 	@Override
 	public Blog get(Blog vo) {
-		logger.info("select one :"+vo);
-		Object[] args = { vo.getId() };
-		return jdbcTemplate.queryForObject(BLOG_GET, args, new BlogRowMapper());
+		// TODO Auto-generated method stub
+		return (Blog)session.selectOne("Blog.selectOne", vo);
 	}
 
 	@Override
 	public List<Blog> list(Blog vo) {
-		logger.info("select all :"+vo);
-		return jdbcTemplate.query(BLOG_LIST, new BlogRowMapper());
+		// TODO Auto-generated method stub
+		return session.selectList("Blog.selectList", vo);
 	}
 
-}
-class BlogRowMapper implements RowMapper<Blog>{
-
-	//ResultMap
-	@Override
-	public Blog mapRow(ResultSet rs, int rowNum) {
-		Blog vo = new Blog();
-		try {
-			vo.setId(rs.getInt("ID"));
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			vo.setTitle(rs.getString("TITLE"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setWname(rs.getString("WNAME"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setContent(rs.getString("CONTENT"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setWdate(rs.getDate("WDATE"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return vo;
-	}
-	
 }
--- src/main/java/com/hakunamatata/springmvc/repository/impl/UserDAO.java
+++ src/main/java/com/hakunamatata/springmvc/repository/impl/UserDAO.java
@@ -1,125 +1,48 @@
 package com.hakunamatata.springmvc.repository.impl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.hakunamatata.springmvc.entity.UserTour;
 import com.hakunamatata.springmvc.repository.DAO;
 
-@Repository("userDAO")
+
+@Repository
 public class UserDAO implements DAO<UserTour> {
-	private static final Logger logger = 
-			LoggerFactory.getLogger(UserDAO.class);
-	
 	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	// SQL
-		private final String USERTOUR_INSERT= "insert into user_tour (username, passwd, cusname, phone, birthday, address, email) values (?,?,?,?,?,?,?)";
-		private final String USERTOUR_UPDATE= "update user_tour set username=?, passwd=?, cusname=?, phone=?, birthday=?, address=?, email=?  where id=?";
-		private final String USERTOUR_DELETE= "delete from user_tour where id=?";//use primary key
-		private final String USERTOUR_GET= "select * from user_tour where id=?";
-		private final String USERTOUR_LIST= "select * from user_tour";
+	private SqlSessionTemplate session;
+
 	@Override
 	public void insert(UserTour vo) {
-		logger.info("insert : "+vo);
-		jdbcTemplate.update(USERTOUR_INSERT, 
-						vo.getUsername(), vo.getPasswd(), vo.getCusname(), vo.getPhone(), vo.getBirthday(), vo.getAddress(), vo.getEmail());
-		
+		// TODO Auto-generated method stub
+		session.insert("UserTour.insert", vo);		
 	}
 
 	@Override
 	public void update(UserTour vo) {
-		logger.info("update :"+vo);
-		jdbcTemplate.update(USERTOUR_UPDATE, 
-				vo.getUsername(), vo.getPasswd(), vo.getCusname(), vo.getPhone(), vo.getBirthday(), vo.getAddress(), vo.getEmail(),
-				vo.getId());
-		
+		// TODO Auto-generated method stub
+		session.update("UserTour.update", vo);
 	}
 
 	@Override
 	public void delete(UserTour vo) {
-		logger.info("delete :"+vo);
-		jdbcTemplate.update(USERTOUR_DELETE, vo.getId());
-		
+		// TODO Auto-generated method stub
+		session.delete("UserTour.delete", vo);
 	}
 
 	@Override
 	public UserTour get(UserTour vo) {
-		logger.info("select one :"+vo);
-		Object[] args = { vo.getId() };
-		return jdbcTemplate.queryForObject(USERTOUR_GET, args, new UserTourRowMapper());
+		// TODO Auto-generated method stub
+		return (UserTour)session.selectOne("Blog.selectOne", vo);
 	}
 
 	@Override
 	public List<UserTour> list(UserTour vo) {
-		logger.info("select all :"+vo);
-		return jdbcTemplate.query(USERTOUR_LIST, new UserTourRowMapper());
+		// TODO Auto-generated method stub
+		return session.selectList("UserTour.selectList", vo);
 	}
 
-}
-class UserTourRowMapper implements RowMapper<UserTour>{
-
-	//ResultMap
-	@Override
-	public UserTour mapRow(ResultSet rs, int rowNum) {
-		UserTour vo = new UserTour();
-		try {
-			vo.setId(rs.getInt("ID"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setUsername(rs.getString("USERNAME"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setPasswd(rs.getString("PASSWD"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setCusname(rs.getString("CUSNAME"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setPhone(rs.getString("PHONE"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setBirthday(rs.getDate("BIRTHDAY"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setAddress(rs.getString("ADDRESS"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			vo.setEmail(rs.getString("EMAIL"));
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		
-		return vo;
-	}
 }
